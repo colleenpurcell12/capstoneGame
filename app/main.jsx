3570b2d1e51373a77457a2160f307b7c5f3d6173
Colleen Purcell <colleenpurcell12@gmail.om>
2016-11-16 16:39:24 -0500

    firebase with bones

--- app/main.jsx
+++ app/main.jsx
@@ -9,6 +9,18 @@ import Jokes from './components/Jokes'
 import Login from './components/Login'
 import WhoAmI from './components/WhoAmI'
 
+import * as firebase from 'firebase'
+
+var config = {
+    apiKey: "AIzaSyD9H4dWewyn6xf8DMJDjNWnFlypG6hg4zU",
+    authDomain: "capstonegame-24bce.firebaseapp.com",
+    databaseURL: "https://capstonegame-24bce.firebaseio.com",
+    storageBucket: "capstonegame-24bce.appspot.com",
+    messagingSenderId: "575027063210"
+  };
+  
+firebase.initializeApp(config);
+
 const ExampleApp = connect(
   ({ auth }) => ({ user: auth })
 ) (
