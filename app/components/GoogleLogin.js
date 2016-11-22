c66c0d6173b0a211714469413343e180431006e1
Deborah Kwon <deborah.m.kwon@gmail.com>
2016-11-22 15:00:42 -0500

    add Drawer, doesn't close on outside click yet

--- app/components/GoogleLogin.js
+++ app/components/GoogleLogin.js
@@ -2,19 +2,27 @@ import React, { Component } from 'react';
 import * as firebase from 'firebase'
 import {Link} from 'react-router';
 import Avatar from 'material-ui/Avatar';
+import FlatButton from 'material-ui/FlatButton';
 import IconButton from 'material-ui/IconButton';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import {Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle} from 'material-ui/Toolbar';
 import IconMenu from 'material-ui/IconMenu';
 import RaisedButton from 'material-ui/RaisedButton';
 import {Nav, Grid, Row, Col} from 'react-bootstrap';
 import DropDownMenu from 'material-ui/DropDownMenu';
+
 export class GoogleLogin extends Component {
 	constructor() {
 	  super()
+    this.state = {
+        rulesOpened: false
+      }
 	  this.signIn = this.signIn.bind(this);
 	  this.signOut = this.signOut.bind(this);
+    this.toggleRules = this.toggleRules.bind(this);
+    this.closeRules = this.closeRules.bind(this);
 
   }
   componentDidMount() {
@@ -28,11 +36,34 @@ export class GoogleLogin extends Component {
 		firebase.auth().signOut();
 	}
 
+  toggleRules(){
+    this.setState({rulesOpened: !this.state.rulesOpened});
+  }
+
+  closeRules(){
+    this.setState({rulesOpened: false});
+  }
+
 	render() {
     return (
             <div>
   					{ this.props.loggedInUser && this.props.loggedInUser.displayName ?
                 <Toolbar>
+                  <ToolbarGroup>
+                    <FlatButton label="Rules" primary={true} style={{textAlign:'left'}}
+                      onTouchTap={() => this.toggleRules}
+                    />
+                    <Drawer
+                      docked={false}
+                      width={200}
+                      open={this.state.rulesOpened}
+                      onRequestChange={(open) => this.setState({rulesOpened: !open})}
+                    >
+                      <MenuItem onTouchTap={() => this.closeRules()}>Rule the First</MenuItem>
+                      <MenuItem onTouchTap={() => this.closeRules()}>Rule the Second</MenuItem>
+                    </Drawer>
+
+                  </ToolbarGroup>
       						<ToolbarGroup style={{textAlign:'center', display: 'inline-block', margin: '0 auto'}}>
       							<ToolbarTitle text={`🌎🚀👽 Welcome to Pioneers of Mars, ${this.props.loggedInUser.displayName.split(" ")[0]}.`} style={{textAlign:'center'}} />
                     <Avatar src={this.props.loggedInUser.photoURL}/>
