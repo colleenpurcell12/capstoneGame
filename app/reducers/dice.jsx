b777c95ddb6f88e3fb78698a93b209c23eba0b1b
Deborah Kwon <deborah.m.kwon@gmail.com>
2016-11-23 15:56:10 -0500

    add dice to state

---
+++ app/reducers/dice.jsx
@@ -0,0 +1,27 @@
+import * as firebase from 'firebase'
+
+/* -----------------    ACTIONS     ------------------ */
+
+const DICE_ROLL = 'DICE_ROLL';
+
+/* ------------   ACTION CREATORS     ------------------ */
+
+const newDiceRoll = diceRoll => ({ type: DICE_ROLL, diceRoll }) //payload is called 'diceRoll'
+
+/* ------------       REDUCER     ------------------ */
+                                //diceRoll is defined one-level deep, but the default could also point to {}
+export default function reducer (diceRoll = {sum: 2, d1:1, d2: 2, isDouble: false}, action) {
+  switch (action.type) {
+    case DICE_ROLL:
+      return action.diceRoll
+    default:
+      return diceRoll;
+  }
+}
+
+
+/* ------------       DISPATCHERS     ------------------ */
+
+export const setDiceRoll = diceRoll => dispatch => {
+  dispatch(newDiceRoll(diceRoll))
+}
