8825c2fc095ec46d7db676a4ab98de12205256c6
Deborah Kwon <deborah.m.kwon@gmail.com>
2016-11-23 15:58:46 -0500

    remove d1 and d2 from state

--- app/reducers/dice.jsx
+++ app/reducers/dice.jsx
@@ -10,7 +10,7 @@ const newDiceRoll = diceRoll => ({ type: DICE_ROLL, diceRoll }) //payload is cal
 
 /* ------------       REDUCER     ------------------ */
                                 //diceRoll is defined one-level deep, but the default could also point to {}
-export default function reducer (diceRoll = {sum: 2, d1:1, d2: 2, isDouble: false}, action) {
+export default function reducer (diceRoll = {sum: 2, isDouble: false}, action) {
   switch (action.type) {
     case DICE_ROLL:
       return action.diceRoll
