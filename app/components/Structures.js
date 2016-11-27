61220edad0131cef551ec8712e7f27f13ef134a2
Colleen Purcell <colleenpurcell12@gmail.om>
2016-11-26 21:05:43 -0500

    outline for validation logic when player attempts to purchase a road or settlement, including isAfforable, isConnected, isAvailable, isFarEnought etc

--- app/components/Structures.js
+++ app/components/Structures.js
@@ -10,12 +10,76 @@ export class Structures extends Component {
     };
   }
   componentDidMount() {
+  }
+  isDuringSetUp(){
+    let {  isSettingUp } = store.getState()
+    return isSettingUp
+  }
+  isAvailable(type,coord){
+    let {  everyStructure } = store.getState()
+    if(type==='road'){
+      let everyRoad = everyStructure.filter( (struc) => struc.type==='road')
+      let sameRoad = everyStructure.filter( (struc) => struc.coordinates===coord)
+      return sameRoad.length!==1
+    }  
+    else { //settlement
+      let everySettlement = everyStructure.filter( (struc) => struc.type==='settlement')
+      let sameSettlement = everyStructure.filter( (struc) => struc.coordinates===coord)
+      return sameSettlement.length!==1
+    }
 
+  }
+  isConnected(coord){
+    //false if there isn't a structure that shares a coordinate
+    let {  everyStructure } = store.getState()
+    // MAP OVER TO CHECK
+    // ...
+  }
+  isAfforable(type){
+    if(type==='road' //cost lumber and 1 brick
+     && userArray[turnInfo][cards][lumber]>=1 && userArray[turnInfo][cards][brick]>=1){
+      return true
+    }  
+    else { //settlement cost 1L+1B+1G+1W
+
+      if( userArray[turnInfo][cards][lumber]>=1 
+        && userArray[turnInfo][cards][brick]>=1
+        && userArray[turnInfo][cards][grain]>=1
+        && userArray[turnInfo][cards][wool]>=1
+        ){
+      return sameSettlement.length!==1
+    }
+  }
+  addingARoad(){
+    // need to know the hexagon state data structure
+    // the user array structure to find the color by ID
+    // ensure that the 1-2 selected corners are stored somewhere
+
+    if( userArray[turnInfo][selection].length===2 && this.isConnected(coord)
+      && this.isAvailable(type,coordinates) && this.isFarEnough() && (this.isAfforable('road') || 
+      (this.isDuringSetUp() && userArray[turnInfo][startRoad]===null) ) ){ //<--no road has been registered/added so far in this set up round, if in set up phase
+
+      let roadObj = {type: 'Road', points: 0, coordinates: this.userArray[turnInfo][selection], associatedHexs = [h1, h2], color: blue, userID: this.turnInfo}]
+
+      this.props.addRoad(roadObj)
+    }
+    else{
+      alert('Please pick two valid end points for your new road and try again')
+    }
+  }
+  addingASettlement(){
+    if( userArray[turnInfo][selection].length===1 && isValidSetUpMove  
+      && this.isAvailable() && this.isFarEnough() && (this.isAfforable('settlement') || 
+      (this.isDuringSetUp() && userArray[turnInfo][startSettlement]===null) ) ){ //<--no settlement has been registered/added so far in this set up round, if in set up phase
+
+    let settlementObj = {type: 'Settlement', points: 1 , coordinates: this.userArray[turnInfo][selection], associatedHexs = [h1, h2, h3],  color: blue, userID: this.turnInfo}]
+
+    this.props.addRoad(settlementObj)
   }
   render() {
     return (
-		<button type='submit' onClick={() => this.registerSettlement}> Add Settlement </button>
-		<button type='submit' onClick={() => this.registerRoad}> Add Road </button>
+		<button type='submit' onClick={() => this.registerSettlement()}> Add Settlement </button>
+		<button type='submit' onClick={() => this.registerRoad()}> Add Road </button>
     	)
 	}
 }
@@ -23,11 +87,12 @@ export class Structures extends Component {
 /* -----------------    CONTAINER     ------------------ */
 
 import {connect} from 'react-redux';
-import { endTurn } from '../reducers/playerStat'; 
+import { addRoad, addSettlement } from '../reducers/everyStructure'; 
 //bring in other results from reducers as necessary**
 
-const mapState = ({ whoseTurn }) => ({whoseTurn});
-const mapDispatch = { endTurn };
+const mapState = ({ turnInfo }) => ({turnInfo}); 
+// might need userArray[turnInfo][selection] or userArray[turnInfo][startRoad]  startSettlement
+const mapDispatch = { addRoad, addSettlement };
 
 export default connect(
   null,
--- app/reducers/robber.js
+++ app/reducers/robber.js
@@ -1,1 +1,26 @@
-robber.js
+import * as firebase from 'firebase'
+
+/* -----------------    ACTIONS     ------------------ */
+
+const MOVE_ROBBER = 'MOVE_ROBBER';
+
+/* ------------   ACTION CREATORS     ------------------ */
+
+const moveRobber = (hexID) => ({ type: MOVE_ROBBER, hexID })
+
+/* ------------       REDUCER     ------------------ */
+                             //whoseTurn is an object that points to a specific player ID, 1 - 4
+export default function reducer (robberHex= 0, action){
+    switch (action.type) {
+    case MOVE_ROBBER:
+      return action.hexID
+    default:
+      return robberHex
+  }
+}
+
+/* ------------      DISPATCHERS     ------------------ */
+
+export const endTurn = (hexID) => dispatch => {
+    dispatch(setNextTurn(hexID));
+}
