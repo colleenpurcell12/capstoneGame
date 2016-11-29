37dc53f7be7be675dd1ccc5feb0f0d7a9ffb803d
Colleen Purcell <colleenpurcell12@gmail.om>
2016-11-29 14:44:52 -0500

    validation of settlement registering

--- app/components/Structures.js
+++ app/components/Structures.js
@@ -13,57 +13,84 @@ export class Structures extends Component {
     };
   }
   componentDidMount() {
+
   }
   isDuringSetUp(){
     let {  isSettingUp } = store.getState()
     return isSettingUp
   }
-  isAvailable(type, coord){
-    return true
-    // let {  everyStructure } = store.getState()
-    // if(type==='road'){
-    //   let everyRoad = everyStructure.filter( (struc) => struc.type==='road')
-    //   let sameRoad = everyStructure.filter( (struc) => struc.coordinates===coord)
-    //   if(!sameRoad){ return true }
-    //   else { return false }
-    // }
-    // else { //settlement
-    //   let everySettlement = everyStructure.filter( (struc) => struc.type==='settlement')
-    //   let sameSettlement = everyStructure.filter( (struc) => struc.coordinates===coord)
-    //   if(!sameSettlement){ return true }
-    //   else { return false }
-    // }
-  }
-  isConnected(coord){
-    //false if there isn't a structure that shares a coordinate
+  isAvailable(type, coord){           
+
+    //return true //for testing
     let {  everyStructure } = store.getState()
-    // MAP OVER TO CHECK
-    // ...
-    return true
+    if(type==='road'){
+      let everyRoad = everyStructure.filter( (struc) => struc.type==='road')
+      console.log("everyRoad have only roads in everyStructure, but it's:", everyRoad)
+      let sameRoad = everyStructure.filter( (struc) => struc.coordinates===coord)
+      console.log("sameRoad should be empty cause no match in everyStructure, but it's:",sameRoad)
+
+      if(!sameRoad){ return true }
+      else { return false }
+    }
+    else { //settlement
+      let everySettlement = everyStructure.filter( (struc) => struc.type==='settlement')
+      console.log("everySettlement have only settlements in everyStructure, but it's:", everySettlement)
+      let sameSettlement = everyStructure.filter( (struc) => struc.coordinates===coord)
+      console.log("sameSettlement should be empty cause no match in everyStructure, but it's:",sameSettlement)
+      if(!sameSettlement){ return true }
+      else { return false }
+    }
+  }
+  isConnected(coord){ //only for registering roads
+
+    //Check if there is a settlement/city or a road that you own, which shares a corner
+    //aka matches on one of the coordinates of the road you are adding, coord.
+    let {  everyStructure } = store.getState()
+    let shareCorner1 = everyStructure.filter( (struc) => struc.coordinates[0]===coord)
+    let shareCorner2 = everyStructure.filter( (struc) => struc.coordinates[1]===coord)
+    return (shareCorner1 || shareCorner2)
+    //return true //for testing
   }
   isAfforable(type, userID){
     let {  userArray } = store.getState()
     let userCards = userArray[userID].cardsResource
     console.log("userCards defined?", userCards)
-    if(type==='road'){ //cost lumber and 1 brick
-      //type1 = lumber type2 = brick
-      //type3 = wool type4 = grain
+    if(type==='road'){ //cost lumber and 1 brick in catan world, not mars world
+      //type1 = lumber, type2 = brick
+      //type3 = wool, type4 = grain
      return userCards.type1>=1 && userCards.type2>=1
-    } else { //settlement cost 1L+1B+1G+1W
-      return (  userCards.type1>=1 && userCards.type2>=1
-              && userCards.type4>=1 && userCards.type3>=1  )
+    } 
+    else { //settlement cost 1L+1B+1G+1W
+      return ( userCards.type1>=1 && userCards.type2>=1
+              && userCards.type4>=1 && userCards.type3>=1 )
     }
   }
-  isFarEnough(type){
-    // if road, neither of the corners of it's end points have non-owned settlements/cities
-    if(type==='road'){
-      //check seleected corner
-    }
+  isFarEnough(coord){
+    let {  turnInfo, selections } = store.getState()
+    var cornerNeighbors1 =  selections[0].neighbors
+    //assumer cornerNeighbors1 is an array with the coordinates of each corner
+
+    // if road, neither of the neighbor corners of it's end points-corners have non-owned settlements/cities
+      
+      // var cornerNeighbors2 =  selections[1].neighbors
+      // selections[1] neighbors is just an array of corner_IDs ...where are the corner coordinates?
+    
+
     // check that neighbors corners dont have non-owned settlements/cities
-    if(type==='settlement'){
+
+      //have too look at each neighbor corner and see if any match another player's owned structures
+      for(var i = 0 ; i<cornerNeighbors1.length ; i++){
+        let tooCloseStructure = everyStructure.filter( (struc) => 
+          (struc.type==='settlement' || struc.type==='city') && userID!==turnInfo
+          //a structure at a corner neighbor owned by someone else
+            ) //closes filter
+      } //closes for loop
+      if( tooCloseStructure ){ 
+        return false
+      }
       // look in everyStructure for settlements/cities
       // then try to "match" the settlements by ??? to the neighbor corners by either name or coordinates
-    }
+    
     return true
   }
   registerRoad(){
@@ -83,7 +110,7 @@ export class Structures extends Component {
       //   y:
       // }
     var coord = [ [selections[0].x, selections[0].y],
-                  [selections[1].x, selections[1].y] ] //[[11,-19],[5,-9]] //x1,y1,x2,y2 
+                  [selections[1].x, selections[1].y] ] //[[11,-19],[5,-9]] //x1,y1,x2,y2              
     let userID = turnInfo
     let userObj = userArray[userID]
     let userColor = userObj.color
@@ -92,14 +119,12 @@ export class Structures extends Component {
     let associatedHexs = associatedHexsCorner1.concat( selections[1].hexes )
 
     console.log("this.isConnected(coord) ",this.isConnected(coord)  )
-    console.log("this.isFarEnough('road')", this.isFarEnough('road') ) 
     console.log("this.isAvailable('road', userID)",this.isAvailable('settlement', coord) )
     console.log("this.isAfforable('road', userID)",this.isAfforable('settlement', userID) )
     console.log("this.isDuringSetUp() && !hasAlreadyPurchased", this.isDuringSetUp() && !hasAlreadyPurchased )
 
     if( selections.length===2 
       && this.isConnected(coord) 
-      && this.isFarEnough('road')
       && this.isAvailable('road',coord)  
       && ( this.isAfforable('road', userID) || (this.isDuringSetUp() && !hasAlreadyPurchased) ) 
       ){ 
@@ -151,7 +176,7 @@ export class Structures extends Component {
     // associated hexes will be corner[0,0,0:0,1,-1:1,0,-1:].hexes
 
     if( selectedCorner.length===1 //&& isValidSetUpMove  
-      && this.isAvailable() && this.isFarEnough('settlement') && (this.isAfforable('settlement') || 
+      && this.isAvailable() && this.isFarEnough('settlement', coord) && (this.isAfforable('settlement') || 
 
       (this.isDuringSetUp() && !alreadyPurchased ) ) ){ //<--no settlement has been registered/added so far in this set up round, if in set up phase
       let settlementObj = { type: 'settlement', points: 1 , color: userColor, userID: userID,
