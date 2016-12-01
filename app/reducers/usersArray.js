9d250ffb9eb5398b714a163b4e1e2a676cf14e4a
Sami Lugar <sami.lugar@gmail.com>
2016-12-01 15:32:48 -0500

    created structure for deal and setupDeal functions

--- app/reducers/usersArray.js
+++ app/reducers/usersArray.js
@@ -13,32 +13,32 @@ export const addPoint = (userIndex) => ({ type: ADD_POINT, userIndex })
 //selection: [ ], taken out since can't select corners if it isn't your turn
 
 let initialUserArray = [
-  	{id: 1, 
-    color: 'red', 
-    hasBoughtARoad: false, 
-    hasBoughtASettlement: false, 
-    points: 3, 
+  	{id: 1,
+    color: 'red',
+    hasBoughtARoad: false,
+    hasBoughtASettlement: false,
+    points: 3,
     cardsResource: {type1: 2 , type2: 2, type3: 3, type4: 3, type5: 3}
     },
  	{id: 2, 
-    color: 'blue', 
-    hasBoughtARoad: false, 
-    hasBoughtASettlement: false, 
-    points: 0, 
+    color: 'blue',
+    hasBoughtARoad: false,
+    hasBoughtASettlement: false,
+    points: 0,
     cardsResource: {type1: 2 , type2: 2, type3: 2, type4: 2, type5: 2}
     },
-    {id: 3, 
-    color: 'green', 
-    hasBoughtARoad: false, 
-    hasBoughtASettlement: false, 
-    points: 0, 
+    {id: 3,
+    color: 'green',
+    hasBoughtARoad: false,
+    hasBoughtASettlement: false,
+    points: 0,
     cardsResource: {type1: 2 , type2: 2, type3: 2, type4: 2, type5: 2}
     },
-    {id: 4, 
-    color: 'yellow', 
-    hasBoughtARoad: false, 
-    hasBoughtASettlement: false, 
-    points: 0, 
+    {id: 4,
+    color: 'brown',
+    hasBoughtARoad: false,
+    hasBoughtASettlement: false,
+    points: 0,
     cardsResource: {type1: 3 , type2: 3, type3: 3, type4: 3, type5: 3}
     }
     ]
