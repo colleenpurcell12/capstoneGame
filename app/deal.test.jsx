0ba1363ff950f9dd1960c1e251a53d39e2d204bb
Sami Lugar <sami.lugar@gmail.com>
2016-12-02 14:36:17 -0500

    added test to check increment resource

--- app/deal.test.jsx
+++ app/deal.test.jsx
@@ -1,22 +1,42 @@
 import {expect} from 'chai'
-import {deal, setupDeal} from 'APP/gameutils/setup'
+import {assignHexInfo, tokenArray, resourcesArray, generate} from 'APP/gameutils/setup'
 import { addAction } from 'APP/app/reducers/action-creators'
+import {incrementResource } from 'APP/app/reducers/players';
+import {boardConfig} from 'APP/gameutils/boardConfig'
+import {deal, setupDea} from 'APP/gameutils/deal'
+import sinon from 'sinon'
 
-// structures, corners, and hexData are passed in from store
-// how to get corners from Board? put on state
-// var roll = this.props.diceRoll.d1 + this.props.diceRoll.d2
-var structures
-var corners
-var hexData 
+let grid = generate(boardConfig);
+var corners = grid.corners
+var hexData = resourcesArray.map((res, i) => {
+  return {token: tokenArray[i], resource: res }
+})
 
 describe('Deal', () => {
-  describe(' loops over the structures array', () => {
-    it('that calls incrementResource for each corners hex that matches token', () => {
-      var roll = 4;
-      deal(structures, corners, hexData, roll)
-      var playerstats = store.getState(players)
-      var total = playerstats[2].cardsTotal()
-      expect(total).to.be.equal(1)
+
+  var roll = hexData[2].token // corner 12 is on hex #3
+  var structures = [ {owner: 'red', corner_id: 12, type: 'settlement', player: 'Sami Lugar' }]
+
+  describe('actions', () => {
+    it('should create an action to increment Resources', () => {
+      let player = structures[0].player
+      let resource = 'crops'
+      const expectedAction = {
+        type: 'INCREMENT_RESOURCE',
+        player,
+        resource,
+        count: 1
+      }
+      expect(incrementResource(player, resource, 1)).to.deep.equal(expectedAction)
     })
   })
+
+  describe(' loops over the structures array', () => {
+    it('that calls incrementResource for each corners hex that matches token', () => {
+      var check = sinon.spy(incrementResource)
+      deal(structures, corners, hexData, roll)
+      expect(check.called).to.be.true
+    })
+  })
+
 })
